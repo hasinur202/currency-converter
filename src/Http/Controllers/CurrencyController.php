0ab39dddd2982243d7757bca431cf6ce9fde70f8
Hasinur Rahman <hasinur202@gmail.com>
2023-06-14 01:57:50 +0600

    fetch the rate of the day from the European Central Bank

--- src/Http/Controllers/CurrencyController.php
+++ src/Http/Controllers/CurrencyController.php
@@ -4,10 +4,20 @@ namespace Skylark\CurrencyConverter\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use SimpleXMLElement;
 
 class CurrencyController extends Controller
 {
-    public function calculateCurrency () {
-        return 'Hello from controller';
+    public function calculateCurrency ($amount, $currency) {
+        $url = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
+        $xmlString = file_get_contents($url);
+
+        $xml = simplexml_load_string($xmlString, "SimpleXMLElement", LIBXML_NOCDATA);
+        $json = json_encode($xml);
+        $array = json_decode($json, true);
+
+        return $array;
+
+
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
 Route::group(['namespace' => 'Skylark\CurrencyConverter\Http\Controllers'], function() {
-    Route::get('currency', 'CurrencyController@calculateCurrency');
+    Route::get('currency/{amount}/{currency}', 'CurrencyController@calculateCurrency');
 });
 
