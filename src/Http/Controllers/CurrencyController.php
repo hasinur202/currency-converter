9e7772e458a2ddc5eed5461447b970f683a4c04d
Hasinur Rahman <hasinur202@gmail.com>
2023-06-14 02:15:42 +0600

    check currency exist or not

--- src/Http/Controllers/CurrencyController.php
+++ src/Http/Controllers/CurrencyController.php
@@ -4,12 +4,38 @@ namespace Skylark\CurrencyConverter\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Arr;
 use SimpleXMLElement;
 
 class CurrencyController extends Controller
 {
     public function calculateCurrency ($amount, $currency) {
-        $url = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
+
+        if ($amount && $currency) {
+            $curr = strtoupper($currency);
+    
+            $url = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
+    
+            $dataArray = $this->fetchDataFromEuropeanBank($url);
+
+            $existCurrency = Arr::first($dataArray, function ($value, $key) use ($curr) {
+                return $value['currency'] == $curr;
+            });
+
+            if (!$existCurrency) {
+                return 'Entered Currency Not Found. Please try for another currency.';
+            } else {
+                return $existCurrency;
+            }
+
+        } else {
+            return 'Please pass the valid amount & currency';
+        }
+    }
+
+
+    
+    public function fetchDataFromEuropeanBank ($url) {
         $xmlString = file_get_contents($url);
 
         $xml = simplexml_load_string($xmlString, "SimpleXMLElement", LIBXML_NOCDATA);
@@ -25,6 +51,5 @@ class CurrencyController extends Controller
         }
 
         return $newArray;
-
     }
 }
