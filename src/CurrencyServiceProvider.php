874eefc07ba778b0fe88a3c2fcfd41d6a69735e2
Hasinur Rahman <hasinur202@gmail.com>
2023-06-14 00:56:44 +0600

    creating a service provier and routes

--- src/CurrencyServiceProvider.php
+++ src/CurrencyServiceProvider.php
@@ -23,6 +23,6 @@ class CurrencyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
     }
 }
---
+++ src/routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('currency', function () {
+    return 'currency';
+});
